b954c1e8de0ec9f30d5e4339dbe9e2370bfee63a
BelovN <nikita.belov.2012@mail.ru>
2020-11-30 11:42:20 +0500

    Визуализация

--- theories/services.py
+++ theories/services.py
@@ -10,6 +10,7 @@ def convert_datetime(data):
     ''' Конвертирует колонки <DATE> и <TIME> в числовой формат для обображения
     '''
     indexes = []
+    converted_dates = []
     for i, date in enumerate(data['<DATE>']):
         if str(data['<TIME>'][i]) == '0':
             full_date = str(data['<DATE>'][i]) + '000000'
@@ -17,17 +18,18 @@ def convert_datetime(data):
             full_date = str(data['<DATE>'][i]) + str(data['<TIME>'][i])
         converted_date = datetime.strptime(full_date, '%Y%m%d%H%M%S')
         indexes.append(converted_date)
-
+        converted_dates.append(converted_date)
     indexes = dates.date2num(indexes)
-    return indexes
+    return indexes, converted_dates
 
 
 def standartize_data(data):
     ''' Удаление лилних колонок и добавление единой колонки DATE (дата и время)
     '''
-    DATE = convert_datetime(data)
+    DATE, CONVERTED_DATE = convert_datetime(data)
+    data = data.assign(CONVDATE=CONVERTED_DATE)
     # data = data.drop(columns=['<DATE>', '<TIME>', '<PER>', '<VOL>', '<TICKER>'])
-    data = data.drop(columns=['<DATE>', '<PER>', '<VOL>', '<TICKER>'])
+    data = data.drop(columns=[ '<PER>', '<VOL>', '<TICKER>'])
     data = data.assign(DATE=DATE)
     return data
 
@@ -53,18 +55,19 @@ def get_standartized_data(path=GAZP_PATH_MIN_2020, count=None):
 def format_data_to_tuple(data):
     ''' Форматирует данные в список кортежей
     '''
-    data_tuples = [tuple([data['DATE'][i],
+    data_tuples = [tuple([i,
                      data['<OPEN>'][i],
                      data['<HIGH>'][i],
                      data['<LOW>'][i],
                      data['<CLOSE>'][i]]) for i in range(len(data))]
+
     return data_tuples
 
 
-def get_tuppled_data():
+def get_tuppled_data(path=GAZP_PATH_HOUR_2018, count=None):
     ''' Внешняя интерфейсная функция для быстрого получения данных для отображения
     '''
-    data = get_standartized_data()
+    data = get_standartized_data(path=path, count=count)
     data_tuples = format_data_to_tuple(data)
 
     return data_tuples
--- theories/view.py
+++ theories/view.py
@@ -1,9 +1,13 @@
 import time
-import keyboard
+import matplotlib as mpl
 import matplotlib.dates as dates
 import matplotlib.pyplot as plt
 from matplotlib import ticker
+import matplotlib.patches as patches
 from mplfinance.original_flavor import candlestick_ohlc
+
+from settings import *
+from services import format_data_to_tuple, get_standartized_data
 
 mpl.use('Qt5Agg')
 
@@ -70,12 +74,17 @@ class BaseViewCandles(SettingsMixinView):
     ''' Базовый класс для динамического отображения графика свечей
     '''
 
-    def __init__(self, data, N=10, **kwargs):
+    def __init__(self, data, N=30, **kwargs):
         ''' Данные должны быть в формате списка кортежей из 5 состовляющих
         '''
         self.data = data
+        self.tuppled_data = format_data_to_tuple(self.data)
         self.N = N
         self.set_up(**kwargs) # Настройки
+
+
+    def date_ticker(self, x, pos):
+        return int(x)
 
 
     def _show_algorithm(self, *args, **kwargs):
@@ -84,22 +93,40 @@ class BaseViewCandles(SettingsMixinView):
         return
 
 
-    def show(self, width=0.0007, alpha=0.8, *args, **kwargs):
+    def show(self, width=0.3, alpha=0.8, *args, **kwargs):
         ''' Базовая функция для отображения функции на графике
         '''
+
+        self.ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.date_ticker))
+        self.ax.set_xticklabels(self.data['CONVDATE'])
+
         plt.ion()
         plt.show(block=False)
-        for i in range(len(self.data)):
+        SMA = self.data['<CLOSE>'].rolling(20).mean()
+        EMA = self.data['<CLOSE>'].ewm(span=20, adjust=False).mean()
+
+        for i in range(len(self.tuppled_data)):
 
             if self.ax.has_been_closed:
                 break
 
-            candles_data = self.data[i:i+self.N*3]
-            candles = candlestick_ohlc(self.ax, candles_data, width=width,
+            candles_data = self.tuppled_data[i:i+self.N]
+            lines, rectangles = candlestick_ohlc(self.ax, candles_data, width=width,
                                        colorup='g', colordown='r', alpha=alpha)
+            # lines[1].set_color("orange")
+            # rectangles[1].set_color("orange")
 
-            x_lim = self.ax.get_xlim()
+
+            self.ax.plot(range(i, i+self.N), SMA[i:i+self.N], color='blue', label='SMA5')
+            self.ax.plot(range(i, i+self.N), EMA[i:i+self.N], color='black', label='EMA5')
+
+            self.ax.legend(loc='best')
+
+            min_lim = min(self.data['<LOW>'].iloc[i:i+self.N])
+            max_lim = max(self.data['<HIGH>'].iloc[i:i+self.N])
+
             self.ax.set_xlim([candles_data[0][0], candles_data[self.N-1][0]])
+            self.ax.set_ylim([min_lim-0.3, max_lim+0.3])
 
             self.fig.canvas.draw()
             self.fig.canvas.flush_events()
@@ -108,3 +135,9 @@ class BaseViewCandles(SettingsMixinView):
 
             self.ax.lines = []
             self.ax.patches = []
+
+
+data = get_standartized_data(path=GAZP_PATH_HOUR_2020)
+
+a =  BaseViewCandles(data)
+a.show()
